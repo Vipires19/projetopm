b22faf1fdf6c966da8d5ddb2f4c2ae5acc823cf8
Vinícius Campos <163905335+Vipires19@users.noreply.github.com>
2024-06-01 16:47:25 -0300

    Update Roubo Veículo.py

--- pages/Roubo Veículo.py
+++ pages/Roubo Veículo.py
@@ -1,4 +1,12 @@
 import streamlit as st
 import pandas as pd
 
-df = st.session_state
+df = st.session_state['data']
+
+colunas = ['mês', 'Dia', 'Dia da semana', 'Hora', 'Período', 'CIA PM', 'Endereço', 'Bairro', 'Subsetor', 'Cidade', 'Crime', 'Tipo do veículo', 'Marca', 'Modelo', 'Placa', 'Ano',
+           'Vítima', 'Flagrante?',  'Localizado?', 'Data da Recuperação', 'Bairro da recuperação', 'Ambiente', 'N° de agressores', 'Sexo dos agressores', 'Cor', 'Arma utilizada', 'Transporte utilizado',
+       'Cor do veículo utilizado']
+
+df = df[colunas]
+
+st.tittle('**Roubo de Veículos 3° BC')
