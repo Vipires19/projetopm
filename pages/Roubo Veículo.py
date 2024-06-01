eade9525d9e7d1ea9697ecc823ed74b11ab9f073
Vinícius Campos <163905335+Vipires19@users.noreply.github.com>
2024-06-01 16:57:13 -0300

    Update Roubo Veículo.py

--- pages/Roubo Veículo.py
+++ pages/Roubo Veículo.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 df = st.session_state['data']
 
-colunas = ['mês', 'Dia', 'Dia da semana', 'Hora', 'Período', 'CIA PM', 'Endereço', 'Bairro', 'Subsetor', 'Crime, 'Cidade', 'Tipo do veículo', 'Marca', 'Modelo', 'Placa', 'Ano',
+colunas = ['mês', 'Dia', 'Dia da semana', 'Hora', 'Período', 'CIA PM', 'Endereço', 'Bairro', 'Subsetor', 'Crime', 'Cidade', 'Tipo do veículo', 'Marca', 'Modelo', 'Placa', 'Ano',
            'Vítima', 'Flagrante?',  'Localizado?']
 #, 'Data da Recuperação', 'Bairro da recuperação', 'Ambiente', 'N° de agressores', 'Sexo dos agressores', 'Cor', 'Arma utilizada', 'Transporte utilizado',
  #      'Cor do veículo utilizado']
