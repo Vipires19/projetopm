43c1a76e6e4499b3e571a1169e449cdd7f48449f
Vinícius Campos <163905335+Vipires19@users.noreply.github.com>
2024-06-01 18:02:19 -0300

    Update Roubo Veículo.py

--- pages/Roubo Veículo.py
+++ pages/Roubo Veículo.py
@@ -30,7 +30,7 @@ df_veic = df_tipo[df_tipo['Placa'] == veics]
 
 st.divider()
 st.header('**Veículo**')
-col1,col2 = st.columns(2)
+col1,col2,col3,col4 = st.columns(4)
 car = list(df_veic['Modelo'])[0]
 col1.markdown(f'**Veículo levado**: {car}')
 placa = list(df_veic['Placa'])
@@ -39,9 +39,9 @@ car_let = placa[0].split(placa[0][3])[0]
 placa = car_let + '-' + car_num
 col2.markdown(f'**Placas**: {placa}')
 year = list(df_veic['Ano'])[0]
-col1.markdown(f'**Ano**: {year}')
+col3.markdown(f'**Ano**: {year}')
 mar = list(df_veic['Marca'])[0]
-col1.markdown(f'**Marca**: {mar}')
+col4.markdown(f'**Marca**: {mar}')
 #tipe = list(df_ocr['Tipo do veículo'])
 #placa = list(df_ocr['Placa'])
 ocr = list(df_veic['Endereço'])[0]
@@ -49,29 +49,33 @@ col1.markdown(f'**Endereço da ocorrência**: {ocr}')
 bairro = list(df_veic["Bairro"])[0]
 col2.markdown(f'**Bairro**: {bairro}')
 sector = list(df_veic['Subsetor'])[0].split('.')[-1]
-col2.markdown(f'**Setor**: {sector}')
+col3.markdown(f'**Setor**: {sector}')
 fla = list(df_veic['Flagrante?'])[0]
-col1.markdown(f'**Flagrante?**: {fla}')
+col4.markdown(f'**Flagrante?**: {fla}')
+
 st.header('**Ocorrência**')
+col1,col2,col3 = st.columns(3)
 vit = list(df_veic['Vítima'])[0]
 col1.markdown(f'**Vítima**: {vit}')
 ladrao = list(df_veic['N° de agressores'])[0]
-col1.markdown(f'**N° de agressores**: {ladrao}')
+col2.markdown(f'**N° de agressores**: {ladrao}')
 se = list(df_veic['Sexo dos agressores'])[0]
-col1.markdown(f'**Sexo dos agressores**: {se}')
+col3.markdown(f'**Sexo dos agressores**: {se}')
 clad = list(df_veic['Cor'])[0]
 col1.markdown(f'**Cor**: {clad}')
 gun = list(df_veic['Arma utilizada'])[0]
-col1.markdown(f'**Arma utilizada**: {gun}')
+col2.markdown(f'**Arma utilizada**: {gun}')
 trans = list(df_veic['Transporte utilizado'])[0]
-col1.markdown(f'**Transporte utilizado**: {trans}')
+col3.markdown(f'**Transporte utilizado**: {trans}')
 cvu = list(df_veic['Cor do veículo utilizado'])[0]
 col1.markdown(f'**Cor do veículo utilizado**: {cvu}')
+
 st.header('**Localização:**')
+col1,col2,col3 = st.columns(3)
 locd = list(df_veic['Localizado?'])[0]
 col1.markdown(f'**Localizado?**: {locd}')
 data_rec = list(df_veic['Data da Recuperação'])[0].date()
-col1.markdown(f'**Data da localização**: {data_rec}')
+col2.markdown(f'**Data da localização**: {data_rec}')
 recu = list(df_veic['Bairro da recuperação'])[0]
-col2.markdown(f'**Bairro da localização**: {recu}')
+col3.markdown(f'**Bairro da localização**: {recu}')
 
