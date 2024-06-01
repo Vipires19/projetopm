e63a485b2422a77884502bfef24382cbaf86f0cc
Vinícius Campos <163905335+Vipires19@users.noreply.github.com>
2024-06-01 16:55:12 -0300

    Update Roubo Veículo.py

--- pages/Roubo Veículo.py
+++ pages/Roubo Veículo.py
@@ -3,13 +3,14 @@ import pandas as pd
 
 df = st.session_state['data']
 
-colunas = ['mês', 'Dia', 'Dia da semana', 'Hora', 'Período', 'CIA PM', 'Endereço', 'Bairro', 'Subsetor', 'Cidade', 'Crime', 'Tipo do veículo', 'Marca', 'Modelo', 'Placa', 'Ano',
-           'Vítima', 'Flagrante?',  'Localizado?', 'Data da Recuperação', 'Bairro da recuperação', 'Ambiente', 'N° de agressores', 'Sexo dos agressores', 'Cor', 'Arma utilizada', 'Transporte utilizado',
-       'Cor do veículo utilizado']
+colunas = ['mês', 'Dia', 'Dia da semana', 'Hora', 'Período', 'CIA PM', 'Endereço', 'Bairro', 'Subsetor', 'Cidade', 'Tipo do veículo', 'Marca', 'Modelo', 'Placa', 'Ano',
+           'Vítima', 'Flagrante?',  'Localizado?']
+#, 'Data da Recuperação', 'Bairro da recuperação', 'Ambiente', 'N° de agressores', 'Sexo dos agressores', 'Cor', 'Arma utilizada', 'Transporte utilizado',
+ #      'Cor do veículo utilizado']
 
 df = df[colunas]
 
-st.title('**Roubo de Veículos 3° BC')
+st.title('**Roubo de Veículos 3° BC**')
 
 df_rv = df[df['Crime'] == 'Roubo']
 df_rv
