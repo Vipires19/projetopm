1a30865e1693d1566a685b3717c0ef7720f16ac7
Vinícius Campos <163905335+Vipires19@users.noreply.github.com>
2024-06-03 13:36:36 -0300

    Update Localização.py

--- pages/Localização.py
+++ pages/Localização.py
@@ -30,49 +30,17 @@ on = st.toggle('Dados')
 if on:
     st.dataframe(df_cia)
 
-veic = df_cia['Placa'].value_counts().index
-veics = col3.selectbox('Veículo', veic)
-df_veic = df_cia[df_cia['Placa'] == veics]
-
-st.divider()
-st.header('**Veículo**')
-col1,col2,col3,col4 = st.columns(4)
-car = list(df_veic['Modelo'])[0]
-col1.markdown(f'**Veículo levado**: {car}')
-placa = list(df_veic['Placa'])
-car_num = placa[0].split(placa[0][2])[1]
-car_let = placa[0].split(placa[0][3])[0]
-placa = car_let + '-' + car_num
-col2.markdown(f'**Placas**: {placa}')
-year = list(df_veic['Ano'])[0]
-col3.markdown(f'**Ano**: {year}')
-mar = list(df_veic['Marca'])[0]
-col4.markdown(f'**Marca**: {mar}')
-fla = list(df_veic['Flagrante?'])[0]
-col4.markdown(f'**Flagrante?**: {fla}')
-vit = list(df_veic['Vítima'])[0]
-col1.markdown(f'**Vítima**: {vit}')
-
-st.header('**Localização:**')
-col1,col2,col3 = st.columns(3)
-locd = list(df_veic['Localizado?'])[0]
-col1.markdown(f'**Localizado?**: {locd}')
-data_rec = list(df_veic['Data da Recuperação'])[0].date()
-col2.markdown(f'**Data da localização**: {data_rec}')
-recu = list(df_veic['Bairro da recuperação'])[0]
-col3.markdown(f'**Bairro da localização**: {recu}')
-
 st.divider()
 st.header('**Bairros**')
-colunas = ['CIA PM', 'mês', 'Dia', 'Hora', 'Endereço', 'Bairro', 'Data da Recuperação', 'Bairro da recuperação', 'Ambiente', 'Crime', 'Cidade', 'Tipo do veículo', 'Modelo', 'Placa', 'Ano',
+colunas = ['CIA PM', 'mês', 'Dia', 'Endereço', 'Bairro', 'Data da Recuperação', 'Bairro da recuperação', 'Ambiente', 'Crime', 'Tipo do veículo', 'Modelo', 'Placa', 'Ano',
            'Flagrante?',  'Localizado?']
 df_loc_set = df[df['Localizado?'] == 'Sim']
 df_loc_set = df_loc_set[colunas]
 
-col1,col2,col3 = st.columns(3)
-cia = df_loc_set['CIA PM'].value_counts().index
-cias = col1.selectbox('Cia', cia)
-df_cia = df_loc_set[df_loc_set['CIA PM'] == cias]
+#col1,col2,col3 = st.columns(3)
+#cia = df_loc_set['CIA PM'].value_counts().index
+#cias = col1.selectbox('Cia', cia)
+#df_cia = df_loc_set[df_loc_set['CIA PM'] == cias]
 bairro = df_cia['Bairro'].value_counts().index
 bairros = col2.selectbox('Bairro', bairro)
 df_bairro = df_cia[df_cia['Bairro'] == bairros]
